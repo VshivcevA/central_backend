73ab2f8bba76e420c9ba188e90a96dd7d832eba2
av <vshivcev.alexandr@gmail.com>
2024-04-22 22:09:40 +0300

    ble server test

--- devices/nanoBle33/node-ble-server.js
+++ devices/nanoBle33/node-ble-server.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const noble = require("@abandonware/noble");
 const roundFloat = require("../../utils/roundFloat");
 const insertInto = require("../../utils/insertInto");
-
-app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
@@ -22,67 +19,61 @@ app.use(function (req, res, next) {
     next();
 });
 
+
+const nanoBle33Config = {
+    ina226: {
+        service: {
+            UUID: '180f',
+        },
+        characteristics: {
+            voltage: {
+                name: 'voltage',
+                UUID: '85891a10454f6dbd7cfff590e0ac1ce9'
+                // UUID: '85891a-1045-4f6d-bd7c-1f590e0ac1ce9'
+            }
+        }
+    },
+    bme280: {
+        service: {
+            UUID: '1b21764b490743caae2d6a6281725975',
+        },
+        characteristics: {
+            temperature: {
+                name: 'temperature',
+                UUID: '8e44a0e83b4b4b2581179a9dd06abce3',
+            },
+            pressure: {
+                name: 'pressure',
+                UUID: '7dcfa0a4b6184fc7a2481aeb9b7fda18'
+            },
+            humidity: {
+                name: 'humidity',
+                UUID: 'dd093747ed37436cb1740f9818a474c9'
+            }
+        }
+    },
+}
+
+const nanoBle33Ina226Data = {}
+const nanoBle33Bme280Data = {}
 app.get('/', function (req, res) {
 
-
-    const nanoBle33Config = {
-        ina226: {
-            service: {
-                UUID: '180f',
-            },
-            characteristics: {
-                voltage: {
-                    name: 'voltage',
-                    UUID: '85891a10454f6dbd7cfff590e0ac1ce9'
-                    // UUID: '85891a-1045-4f6d-bd7c-1f590e0ac1ce9'
-                }
-            }
-        },
-        bme280: {
-            service: {
-                UUID: '1b21764b490743caae2d6a6281725975',
-            },
-            characteristics: {
-                temperature: {
-                    name: 'temperature',
-                    UUID: '8e44a0e83b4b4b2581179a9dd06abce3',
-                },
-                pressure: {
-                    name: 'pressure',
-                    UUID: '7dcfa0a4b6184fc7a2481aeb9b7fda18'
-                },
-                humidity: {
-                    name: 'humidity',
-                    UUID: 'dd093747ed37436cb1740f9818a474c9'
-                }
-            }
-        },
-    }
-
-        const nanoBle33Ina226Data = {}
-        const nanoBle33Bme280Data = {}
-
-        console.log('ble start')
         noble.on('stateChange', async (state) => {
-            console.log('state 1')
-
             if (state === 'poweredOn') {
                 await noble.startScanningAsync([nanoBle33Config.ina226.service.UUID, nanoBle33Config.bme280.service.UUID]);
             }
         });
 
         noble.on('discover', async (peripheral) => {
-            console.log('discover');
+            console.log('discover')
             await noble.stopScanningAsync();
-
             await peripheral.connectAsync();
-            console.log('connect')
             const {services, characteristics} = await peripheral.discoverSomeServicesAndCharacteristicsAsync([nanoBle33Config.ina226.service.UUID, nanoBle33Config.bme280.service.UUID], [nanoBle33Config.ina226.characteristics.voltage.UUID, nanoBle33Config.bme280.characteristics.temperature.UUID, nanoBle33Config.bme280.characteristics.humidity.UUID, nanoBle33Config.bme280.characteristics.pressure.UUID])
+            // const {services} = await peripheral.discoverServicesAsync([nanoBle33Config.ina226.service.UUID, nanoBle33Config.bme280.service.UUID])
             for (const service of services) {
                 if (service.uuid === nanoBle33Config.ina226.service.UUID) {
                     for (const characteristic of service.characteristics) {
                         let name = ''
-
                         switch (characteristic.uuid) {
                             case nanoBle33Config.ina226.characteristics.voltage.UUID:
                                 name = nanoBle33Config.ina226.characteristics.voltage.name
@@ -90,53 +81,36 @@ app.get('/', function (req, res) {
                         nanoBle33Ina226Data[name] = (await characteristic.readAsync()).readFloatLE()
                     }
                     roundFloat(nanoBle33Ina226Data)
-                    // console.log(nanoBle33Ina226Data)
                     const table = "nanoble33battery"
                     const columns = ['voltage']
-                    // await insertInto(nanoBle33Ina226Data, table, columns)
-                    console.log(nanoBle33Ina226Data)
-
+                    await insertInto(nanoBle33Ina226Data, table, columns)
                 }
 
-                // if (service.uuid === nanoBle33Config.bme280.service.UUID) {
-                //     // console.log('bme280')
-                //     for (const characteristic of service.characteristics) {
-                //         let name = ''
-                //
-                //         switch (characteristic.uuid) {
-                //             case nanoBle33Config.bme280.characteristics.temperature.UUID:
-                //                 name = 'temperature'
-                //                 break
-                //             case nanoBle33Config.bme280.characteristics.humidity.UUID:
-                //                 name = 'humidity'
-                //                 break
-                //             case nanoBle33Config.bme280.characteristics.pressure.UUID:
-                //                 name = 'pressure'
-                //         }
-                //         nanoBle33Bme280Data[name] = (await characteristic.readAsync()).readFloatLE()
-                //
-                //     }
-                //     roundFloat(nanoBle33Bme280Data)
-                //     // console.log(nanoBle33Bme280Data)
-                //     const table = "climate_2"
-                //     const columns = ['temperature', 'pressure', 'humidity']
-                //     // await insertInto(nanoBle33Bme280Data, table, columns)
-                //     console.log(nanoBle33Bme280Data)
-                //
-                // }
-
+                if (service.uuid === nanoBle33Config.bme280.service.UUID) {
+                    for (const characteristic of service.characteristics) {
+                        let name = ''
+                        switch (characteristic.uuid) {
+                            case nanoBle33Config.bme280.characteristics.temperature.UUID:
+                                name = 'temperature'
+                                break
+                            case nanoBle33Config.bme280.characteristics.humidity.UUID:
+                                name = 'humidity'
+                                break
+                            case nanoBle33Config.bme280.characteristics.pressure.UUID:
+                                name = 'pressure'
+                        }
+                        nanoBle33Bme280Data[name] = (await characteristic.readAsync()).readFloatLE()
+                    }
+                    roundFloat(nanoBle33Bme280Data)
+                    const table = "climate_2"
+                    const columns = ['temperature', 'pressure', 'humidity']
+                    await insertInto(nanoBle33Bme280Data, table, columns)
+                }
             }
             await peripheral.disconnectAsync()
-            console.log('disconnect')
-            // process.exit(0);
-
+            res.json(nanoBle33Ina226Data)
+            process.exit(0);
         });
-
-        console.log('exit')
-res.get('ble')
-// getNanoBle33Bme280Data()
-//     module.exports = getNanoBle33Bme280Data
-
 })
 
-app.listen(3180, () => console.log('server BLE started'));
+app.listen(3080, () => console.log('server BLE started'));
--- shedule/scheduleEvery10Minutes.js
+++ shedule/scheduleEvery10Minutes.js
@@ -1,14 +1,17 @@
+const schedule = require('node-schedule');
 const getBme280Data = require("../devices/esp32_1/getBme280Data");
 const getNanoBle33data = require("../devices/nanoBle33/getNanoBle33data");
 
 function scheduleEvery10Minutes() {
-    const schedule = require('node-schedule');
     const rule = new schedule.RecurrenceRule();
     rule.minute = [0, 10, 20, 30, 40, 50];
     // rule.second = [0, 10, 20, 30, 40, 50];
     const job = schedule.scheduleJob(rule, function () {
         console.log('--- schedule')
         getBme280Data()
+        fetch('http://192.168.10.49:3080')
+            .then(()=>{console.log('ble get')})
+            .catch(()=>{console.log('ble error')})
         // getNanoBle33data()
     });
 }
