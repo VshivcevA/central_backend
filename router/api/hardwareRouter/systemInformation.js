eff7d168844a7035f9fc403a8926c6eec7092031
aleksandrvshivtcev <vshivcev.alexandr@gmail.com>
2024-07-27 15:27:12 +0300

    Добавлены новые параметры для получения информации о системе.

--- router/api/hardwareRouter/systemInformation.js
+++ router/api/hardwareRouter/systemInformation.js
@@ -2,31 +2,19 @@ const si = require('systeminformation');
 
 async function systemInformation() {
     let systemInfoData = []
-    // systemInfoData['uptime']= await si.time().uptime
     let valueObject = {
-        cpu: '*',
-        osInfo: 'platform, release',
-        system: 'model, manufacturer'
+        time:'current,uptime,timezone',
+        cpu: 'vendor,manufacturer,cores',
+        cpuCurrentSpeed:'*',
+        cpuTemperature:'main',
+        mem: 'total,used,free,active,available,buffers,cached,buffcache,swaptotal,swapused',
+        osInfo:'platform,distro,release,codename,kernel',
+        currentLoad:'currentLoad',
+        disksIO:"*",
+        fsSize:"*",
     }
-    // await si.getAllData()
     await si.get(valueObject)
         .then(result => systemInfoData = result);
-    // console.log(systemInfoData)
-    await si.getAllData()
-        .then(result => systemInfoData = result)
     return systemInfoData
 }
 module.exports = systemInformation;
-
-
-// function usersCallback(data) {
-//     console.log('Power usage now: ' + (data.battery.acconnected ? 'AC' : 'battery'));
-// }
-//
-// // now define the observer function
-// let observer = si.observe(valueObject, 1000, usersCallback);
-//
-// // In this example we stop our observer function after 30 seconds
-// setTimeout(() => {
-//     clearInterval(observer)
-// }, 30000);
